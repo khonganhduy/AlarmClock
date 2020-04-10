a28fd5e9f33d0bfe3bb9f53a3834fcc2c0ea0f19
Steven <tangsteven90@gmail.com>
2020-04-09 19:38:14 -0700

    added getter setters

--- app/src/main/java/sjsu/android/alarmclockplusplus/Alarm.java
+++ app/src/main/java/sjsu/android/alarmclockplusplus/Alarm.java
@@ -25,5 +25,40 @@ public class Alarm {
 
     @ColumnInfo(name = "snooze_mode")
     public boolean snoozeMode;
+
+    public Calendar getAlarmTime(){
+        return alarmTime;
+    }
+    public void setAlarmTime(Calendar alarmTime){
+        this.alarmTime = alarmTime
+    }
+
+    public String getRingtonePath(){
+        return ringtonePath;
+    }
+    public void setRingtonePath(String ringtonePath){
+        this.ringtonePath = ringtonePath;
+    }
+
+    public String getRepeatableDays(){
+        return repeatableDays;
+    }
+    public void setRepeatableDays(String repeatableDays){
+        this.repeatableDays = repeatableDays;
+    }
+
+    public String getTriggerDate(){
+        return triggerDate;
+    }
+    public void setTriggerDate(String triggerDate){
+        this.triggerDate = triggerDate;
+    }
+
+    public boolean isSnoozeMode(){
+        return snoozeMode;
+    }
+    public void setSnoozeMode(boolean snoozeMode){
+        this.snoozeMode = snoozeMode;
+    }
 }
 
